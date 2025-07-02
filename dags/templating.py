76f620ab235ff2d51d6d9d0f82df7288452bffdd
klima7 <github@mail.ukasz.com>
2025-07-02 23:34:47 +0200

    Add dag with templating demo

---
+++ dags/templating.py
@@ -0,0 +1,35 @@
+from airflow.sdk import dag, task
+from airflow.providers.standard.operators.python import PythonOperator
+
+
+def get_number():
+    return 42
+
+def add_one(num):
+    return int(num) + 1
+
+@dag(
+    params={"num": 4},
+    user_defined_macros={"get_number": get_number},
+    user_defined_filters={"add_one": add_one},
+    default_args={"author": "Łukasz Klimkiewicz"},
+)
+def templating_dag():
+    
+    def _extract_data(a, current_dag_run_date, templates_dict):
+        print(a, current_dag_run_date)
+        print(templates_dict['new_variable'])
+
+    PythonOperator(
+        task_id="extract_task",
+        python_callable=_extract_data,
+        op_args=["{{ds}}"],
+        op_kwargs={
+            "current_dag_run_date": "{{ds}}"
+        },
+        templates_dict={
+            "new_variable": "Ala ma kota {{get_number()}} {{6 | add_one}}",
+        },
+    )
+    
+templating_dag()
